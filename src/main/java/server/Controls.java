e2c9c8e9d70f3853323ea8ca459f751447725ef0
Tim Lorenz <tim.lorenz@scoop-software.de>
2021-03-02 11:50:16 +0100

    LernFeldEinsTiLo - 1 Added RestServer and Controls

---
+++ src/main/java/server/Controls.java
@@ -0,0 +1,14 @@
+package server;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class Controls {
+}
---
+++ src/main/java/server/RestServer.java
@@ -0,0 +1,40 @@
+package server;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+
+public class RestServer {
+    public static void main(String[] args) throws Exception {
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+
+        Server jettyServer = new Server(8080);
+        jettyServer.setHandler(context);
+
+        ServletHolder jerseysServlet = context.addServlet(
+                org.glassfish.jersey.servlet.ServletContainer.class, "/*");
+        jerseysServlet.setInitOrder(0);
+
+        jerseysServlet.setInitParameter(
+                "jersey.config.server.provider.classnames",
+                Controls.class.getCanonicalName());
+
+        ServletHolder holdHome = new ServletHolder("static-home", DefaultServlet.class);
+        holdHome.setInitParameter("resourceBase", "src/main/resources/static");
+        holdHome.setInitParameter("dirAllowed", "true");
+        holdHome.setInitParameter("pathInfoOnly", "true");
+        context.addServlet(holdHome, "/static/*");
+
+        try {
+            jettyServer.start();
+            jettyServer.join();
+        } finally {
+            jettyServer.destroy();
+        }
+
+    }
+}
